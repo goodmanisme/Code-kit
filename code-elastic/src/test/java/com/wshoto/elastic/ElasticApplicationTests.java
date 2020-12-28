f1db77538525ecc8c53dc7b5177f695261fa22c8
AGoodMan <643012918@qq.com>
2020-12-28 16:10:39 +0800

    elastic初始化

--- code-elastic/src/test/java/com/wshoto/elastic/ElasticApplicationTests.java
+++ code-elastic/src/test/java/com/wshoto/elastic/ElasticApplicationTests.java
@@ -3,11 +3,26 @@ package com.wshoto.elastic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
-class ElasticApplicationTests {
+public class ElasticApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("1",123);
+        map.put("2",456);
+
+        map.remove("1");
+
+
+        System.out.println(map.get("1"));
+        System.out.println(map.get("2"));
+
+
+
     }
 
 }
